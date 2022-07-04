506215aa9c714232b2bc04fd2ebbff1d7688443d
sujata patil <sujatapatil1195@gmail.com>
2022-07-04 13:21:45 +0530

    first commit

---
+++ DemoJava/src/test/Addidtion.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Addidtion {
+
+	public static void main(String[] args) {
+		int a=10,b=7;
+		System.out.println("A+B=" +(a+b));
+	}
+
+}
---
+++ DemoJava/src/test/Substraction.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Substraction
+
+{
+	public static void main(String[] args) {
+	int a=10,b=7;
+	System.out.println("A-B=" +(a-b));
+
+}
+}
